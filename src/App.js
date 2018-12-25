7304ce533df75040e4925f3d6c044e76850271ac
George Andersen <george.andersen@obextech.com>
2018-12-25 12:44:43 +0000

    First completed version. Beaut smooth text validation, buttons (with shortcuts) for Check and reveal the verse, memory.txt file loading in and loop navigation.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
 import React, { Component } from "react";
-// import ReactDOM from 'react-dom';
-import logo from "./logo.svg";
 import Tas from "./Tas.js";
 import "./App.css";
 
-
-let yoe = "https://www.youtube.com/watch?v=3PCHyHvLr4M";
 class App extends Component {
   render() {
     return (
       <div className="App">
 
         <header className="App-header">
-          {/*<img src={logo} className="App-logo" alt="logo" />*/}
-          <h3>Tas Title</h3>
-          {/*<Link link={yoe} title="Listen to Yoe Mase"/>*/}
-          {/*<Link link="https://rainymood.com/" title="Rainy mood"/> */}
+
+          <h3>mem type</h3>
+
           <p></p>
+
         </header>
 
         <Tas/>
@@ -24,12 +20,6 @@ class App extends Component {
       </div>
     );
   }
-}
-
-function Link(props) {
-  return(
-      <a className="App-link" href={props.link}>{props.title}</a>
-    )
 }
 
 export default App;
