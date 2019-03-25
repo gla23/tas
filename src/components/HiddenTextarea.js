75457e21867cc8722441ba0fc90b03bf0e22b08e
George Andersen <george.andersen@obextech.com>
2019-03-25 09:17:46 +0000

    more refactoring of Tas.js and related components

--- src/components/HiddenTextarea.js
+++ src/components/HiddenTextarea.js
@@ -1,23 +1,22 @@
 import React from "react";
 
-
-export default props => (
-	<textarea
-		id="textarea"
-		value={props.text}
-		onChange={props.onChange}
-
-		onSelect={props.updateSelection}
-		onKeyPress={props.updateSelection}
-		onKeyUp={props.updateSelection}
-		onInput={props.updateSelection}
-
-		onKeyDown={props.onKeyDown}
-		spellCheck={false}
-		onFocus={() => props.onChangeFocus(true)}
-		onBlur={() => props.onChangeFocus(false)}
-		
-		autoFocus={true}
-		placeholder={""}
-	/>
-);
+export default props => {
+	let nothing = () => "why";
+	return (
+		<textarea
+			id="textarea"
+			value={props.text}
+			onChange={props.onChange || nothing}
+			onSelect={props.updateSelection}
+			onKeyPress={props.updateSelection}
+			onKeyUp={props.updateSelection}
+			onInput={props.updateSelection}
+			onKeyDown={props.onKeyDown}
+			spellCheck={false}
+			onFocus={() => props.onChangeFocus(true)}
+			onBlur={() => props.onChangeFocus(false)}
+			autoFocus={true}
+			placeholder={""}
+		/>
+	);
+};
